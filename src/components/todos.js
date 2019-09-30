e673185eec67f0fd94735c0c29deb7bdff1eaeae
Yakoob Hayat <yakoob.hayat@fdmgroup.com>
2019-09-30 18:17:42 +0100

    init commit

---
+++ src/components/todos.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+
+class Todos extends React.Component{
+
+  render(){
+    return (
+          <div>
+              <h1>badopalamicon</h1>
+          </div>
+        );
+    }
+}
+
+
+// function App() {
+//   
+// }
+
+export default Todos;
